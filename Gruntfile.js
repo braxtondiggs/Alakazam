90428efb8e57f3dc1bf5508b34effb8ec491b1a8
Braxton Diggs <braxtondiggs@gmail.com>
2016-02-11 23:07:32 -0500

    Update App Name
    Update App Name for diet

--- Gruntfile.js
+++ Gruntfile.js
@@ -371,7 +371,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'gallupApp',
+          module: 'alakazamApp',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
