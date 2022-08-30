de7e87a75fb58cc7acc028f22012b01dbd2aa3ea
Braxton Diggs <braxtondiggs@gmail.com>
2022-08-30 17:39:52 -0400

    github pages deploy

--- functions/lib/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const functions = require("firebase-functions");
-const app = dialogflow({ debug: true });
-exports.fulfillment = functions.https.onRequest(app);
-//# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++
@@ -1,7 +0,0 @@
-'use strict';
-import * as functions from 'firebase-functions';
-import { BasicCard, Button, DialogflowConversation, Image, List, SimpleResponse } from 'actions-on-google';
-
-const app = dialogflow({ debug: true });
-
-export const fulfillment = functions.https.onRequest(app);
